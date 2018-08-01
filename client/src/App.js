b476a92826e94ab822678fa533778ec33955e611
emitsianis <ev.mitsianis@gmail.com>
2018-08-01 19:23:22 +0300

    removed logout timeout

--- client/src/App.js
+++ client/src/App.js
@@ -37,16 +37,16 @@ if (localStorage.jwtToken) {
   //Set user and isAuthenticated
   store.dispatch(setCurrentUser(decoded));
 
-  //Check for expired token
-  const currentTime = Date.now() / 1000;
-  if (decoded.exp < currentTime) {
-    //Logout user
-    store.dispatch(logoutUser());
-    //Clear current profile
-    store.dispatch(clearCurrentProfile());
-    //Redirect to login
-    window.location.href = "/login";
-  }
+  //   //Check for expired token
+  //   const currentTime = Date.now() / 1000;
+  //   if (decoded.exp < currentTime) {
+  //     //Logout user
+  //     store.dispatch(logoutUser());
+  //     //Clear current profile
+  //     store.dispatch(clearCurrentProfile());
+  //     //Redirect to login
+  //     window.location.href = "/login";
+  //   }
 }
 
 class App extends Component {
